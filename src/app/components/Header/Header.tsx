eda0356b7393475a077c5822df08e9c7b5e4fc1f
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-09 12:18:45 -0400

    update header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import DesktopMenu from '../DesktopMenu'
 export default function Header() {
   return (
     <div
-      className="bg-indigo-300/70 items-center h-16 backdrop-blur-sm fixed w-full top-0 flex justify-between border-b border-b-indigo-300 px-8
+      className="z-1 bg-indigo-300/20 items-center h-16 backdrop-blur-sm fixed w-full top-0 flex justify-between border-b border-b-indigo-300 px-8
     ">
       <div className="w-9 h-9">
         <Image src="/logo.ico" alt="logo" width="64" height="64" />
