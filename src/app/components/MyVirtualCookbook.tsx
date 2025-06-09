51f50a6c91707fb69e102c2654b93fc71da0c455
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-09 13:42:15 -0400

    use image buttons for portfolio links

--- src/app/components/MyVirtualCookbook.tsx
+++ src/app/components/MyVirtualCookbook.tsx
@@ -5,6 +5,7 @@ export default function MyVirtualCookbook() {
     <PortfolioItem
       title="MyVirtualCookbook"
       url="https://my-virtual-cookbook.vercel.app/"
+      gitHubUrl="https://github.com/jamiemcg10/MyVirtualCookbook"
       description="MyVirtualCookbook is a web app that lets users add links to recipes to their cookbook and organize them into sections called chapters. Users can add notes to recipes which are saved and tied to the recipe. MyVirtualCookbook was built using React, Next.js, Firebase Authentication, and Cloud Firestore."
       posterSrc="MyVirtualCookbookPoster.png"
       src="MyVirtualCookbook.mp4"
--- src/app/components/PortfolioItem.tsx
+++ src/app/components/PortfolioItem.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Subheading from './Subheading'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import LinkIcon from '@mui/icons-material/Link'
+import IconButton from '@mui/material/IconButton'
 
 interface PortfolioItemProps {
   title: string
   url: string
+  gitHubUrl: string
   description: string
   posterSrc: string
   src: string
@@ -12,6 +18,7 @@ interface PortfolioItemProps {
 export default function PortfolioItem({
   title,
   url,
+  gitHubUrl,
   description,
   posterSrc,
   src,
@@ -26,9 +33,12 @@ export default function PortfolioItem({
       </div>
       <div className="basis-1/2 sm:pl-6 mt-4 sm:mt-0">
         <Subheading>{title}</Subheading>
-        <a href={url} target="_blank" className="text-sm underline italic">
-          {url}
-        </a>
+        <IconButton href={url} target="_blank" size="small">
+          <LinkIcon sx={{ color: 'white' }} />
+        </IconButton>
+        <IconButton href={gitHubUrl} target="_blank" size="small">
+          <GitHubIcon sx={{ color: 'white' }} />
+        </IconButton>
         <div className="text-sm mt-4">{description}</div>
       </div>
     </div>
