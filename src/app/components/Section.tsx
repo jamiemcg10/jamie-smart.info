fc1167936768c9ea7e992c4defc1fdf9a42e4de8
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-09 12:28:52 -0400

    account for header in link

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -8,11 +8,12 @@ interface SectionProps {
 
 export default function Section({ title, href, children }: SectionProps) {
   return (
-    <div
-      id={href}
-      className="mb-4 p-8 border rounded-md bg-indigo-300 border-[#001024] dark:border-indigo-300 dark:bg-[#001024]">
-      <div className="text-3xl mb-4 font-semibold">{title}</div>
-      <div className="space-y-8">{children}</div>
+    <div className="relative">
+      <div className="absolute -mt-20" id={href}></div>
+      <div className="mb-4 p-8 border rounded-md bg-indigo-300 border-[#001024] dark:border-indigo-300 dark:bg-[#001024]">
+        <div className="text-3xl mb-4 font-semibold">{title}</div>
+        <div className="space-y-8">{children}</div>
+      </div>
     </div>
   )
 }
