a66013730f57387d415cf824480928eb706ab6ac
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-11 01:18:01 -0400

    fix experience spacing

--- src/app/components/SkillsAndExperience/Experience.tsx
+++ src/app/components/SkillsAndExperience/Experience.tsx
@@ -4,9 +4,9 @@ import Latitude from './Latitude'
 
 export default function Experience() {
   return (
-    <div className="relative">
+    <div className="relative sm:w-2/3">
       <div className="absolute -mt-12" id="experience"></div>
-      <div className="w-full sm:w-2/3 mt-8 sm:mt-0" id="experience">
+      <div className="w-full mt-8 sm:mt-0" id="experience">
         <Subheading>Experience</Subheading>
         <div className="mt-6">
           <Latitude />
