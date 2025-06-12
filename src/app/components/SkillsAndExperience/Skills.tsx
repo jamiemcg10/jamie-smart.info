b6684620e9b5b5a0434942c82f938d267899f315
Jamie Smart <jamie.smart.10@gmail.com>
2025-06-12 17:53:00 -0400

    show white version when necessaruu

--- src/app/components/SkillsAndExperience/Skills.tsx
+++ src/app/components/SkillsAndExperience/Skills.tsx
@@ -11,48 +11,58 @@ export default function Skills() {
           <div>
             <div className="my-3 text-center">Languages</div>
             <div className="grid grid-cols-4 gap-3">
-              <Skill imgSrc="/skills/typescript.svg">Typescript</Skill>
-              <Skill imgSrc="/skills/javascript.svg">Javascript</Skill>
-              <Skill imgSrc="/skills/python.svg">Python</Skill>
-              <Skill imgSrc="/skills/java.svg">Java</Skill>
-              <Skill imgSrc="/skills/html.svg">HTML5</Skill>
-              <Skill imgSrc="/skills/css.svg">CSS</Skill>
-              <Skill imgSrc="/skills/scss.svg">SCSS</Skill>
+              <Skill imgSrc="/skills/typescript">Typescript</Skill>
+              <Skill imgSrc="/skills/javascript">Javascript</Skill>
+              <Skill imgSrc="/skills/python">Python</Skill>
+              <Skill imgSrc="/skills/java">Java</Skill>
+              <Skill imgSrc="/skills/html">HTML5</Skill>
+              <Skill imgSrc="/skills/css">CSS</Skill>
+              <Skill imgSrc="/skills/scss">SCSS</Skill>
             </div>
           </div>
           <div>
             <div className="my-3 text-center">Frontend</div>
             <div className="grid grid-cols-4 gap-3">
-              <Skill imgSrc="/skills/svelte.svg">Svelte</Skill>
-              <Skill imgSrc="/skills/svelte.svg">SvelteKit</Skill>
-              <Skill imgSrc="/skills/reactjs.svg">React.js</Skill>
-              <Skill imgSrc="/skills/next.svg">Next.js</Skill>
+              <Skill imgSrc="/skills/svelte">Svelte</Skill>
+              <Skill imgSrc="/skills/svelte">SvelteKit</Skill>
+              <Skill imgSrc="/skills/reactjs">React.js</Skill>
+              <Skill imgSrc="/skills/next" dualTone>
+                Next.js
+              </Skill>
             </div>
           </div>
           <div>
             <div className="my-3 text-center">Backend</div>
             <div className="grid grid-cols-4 gap-3">
-              <Skill imgSrc="/skills/express.svg">Express.js</Skill>
-              <Skill imgSrc="/skills/node.svg">Node.js</Skill>
-              <Skill imgSrc="/skills/firestore.svg">Cloud Firestore</Skill>
-              <Skill imgSrc="/skills/elasticsearch.svg">ElasticSearch</Skill>
-              <Skill imgSrc="/skills/postgresql.svg">PostgreSQL</Skill>
-              <Skill imgSrc="/skills/mysql.svg">MySQL</Skill>
-              <Skill imgSrc="/skills/mongodb.svg">MongoDB</Skill>
+              <Skill imgSrc="/skills/express" dualTone>
+                Express.js
+              </Skill>
+              <Skill imgSrc="/skills/node">Node.js</Skill>
+              <Skill imgSrc="/skills/firestore">Cloud Firestore</Skill>
+              <Skill imgSrc="/skills/elasticsearch">ElasticSearch</Skill>
+              <Skill imgSrc="/skills/postgresql">PostgreSQL</Skill>
+              <Skill imgSrc="/skills/mysql">MySQL</Skill>
+              <Skill imgSrc="/skills/mongodb">MongoDB</Skill>
             </div>
           </div>
           <div>
             <div className="my-3 text-center">Tools & Platforms</div>
             <div className="grid grid-cols-4 gap-3">
-              <Skill imgSrc="/skills/git.svg">Git</Skill>
-              <Skill imgSrc="/skills/github.svg">GitHub</Skill>
-              <Skill imgSrc="/skills/cypress.svg">Cypress</Skill>
-              <Skill imgSrc="/skills/playwright.svg">Playwright</Skill>
-              <Skill imgSrc="/skills/figma.svg">Figma</Skill>
-              <Skill imgSrc="/skills/linear.svg">Linear</Skill>
-              <Skill imgSrc="/skills/tailwind-css.svg">Tailwind CSS</Skill>
-              <Skill imgSrc="/skills/material-ui.svg">Material UI</Skill>
-              <Skill imgSrc="/skills/bootstrap.svg">Bootstrap</Skill>
+              <Skill imgSrc="/skills/git">Git</Skill>
+              <Skill imgSrc="/skills/github" dualTone>
+                GitHub
+              </Skill>
+              <Skill imgSrc="/skills/cypress" dualTone>
+                Cypress
+              </Skill>
+              <Skill imgSrc="/skills/playwright">Playwright</Skill>
+              <Skill imgSrc="/skills/figma">Figma</Skill>
+              <Skill imgSrc="/skills/linear" dualTone>
+                Linear
+              </Skill>
+              <Skill imgSrc="/skills/tailwind-css">Tailwind CSS</Skill>
+              <Skill imgSrc="/skills/material-ui">Material UI</Skill>
+              <Skill imgSrc="/skills/bootstrap">Bootstrap</Skill>
             </div>
           </div>
         </div>
