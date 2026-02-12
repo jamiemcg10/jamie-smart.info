38b76ef03a61e63b626654bba740cf10b55b3418
Jamie Smart <jamie.smart.10@gmail.com>
2026-02-12 12:35:23 -0500

    start hero

--- src/app/components/Header/Hero.tsx
+++ src/app/components/Header/Hero.tsx
@@ -1,9 +1,13 @@
 export default function Hero() {
   return (
-    <div className="h-60 bg-[green]">
-      <h1>Jamie Smart</h1>
-      <h2>Software Engineer</h2>
-      <p>Building modern web experiences with React, Next.js, Node.js, and Python</p>
-    </div>
+    <section className="relative h-[380px] bg-gradient-to-br from-indigo-500 via-purple-500 to-blue-400  flex items-center justify-between px-8 md:px-20 py-20">
+      <div>
+        <h1 className="text-5xl font-bold mb-2">Jamie Smart</h1>
+        <h2 className="text-2xl opacity-90 mb-6">Software Engineer</h2>
+        <p className="mb-8 opacity-90">
+          Building modern web experiences with React, Next.js, Node.js, and Python
+        </p>
+      </div>
+    </section>
   )
 }
