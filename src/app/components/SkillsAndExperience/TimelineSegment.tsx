7d95be213101eda8ab9fe0cc2b673844eb4be4b9
Jamie Smart <jamie.smart.10@gmail.com>
2026-02-12 14:54:55 -0500

    remove unused files

--- src/app/components/SkillsAndExperience/TimelineSegment.tsx
+++
@@ -1,8 +0,0 @@
-export default function TimelineSegment() {
-  return (
-    <div className="flex flex-col py-2">
-      <div className="bg-green-950 group-hover:bg-green-700 group-hover:shadow-(--timeline-shadow) transition-all border-3 dark:border-white rounded-4xl w-4 h-4"></div>
-      <div className="w-px place-self-center border-r-4 dark:border-r-white grow group-hover:shadow-(--timeline-shadow) transition-all"></div>
-    </div>
-  )
-}
