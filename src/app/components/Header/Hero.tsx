3d26a03495cba1b1907ce9b44344c42e25d7bb7d
Jamie Smart <jamie.smart.10@gmail.com>
2026-02-13 00:48:20 -0500

    update light hero gradient

--- src/app/components/Header/Hero.tsx
+++ src/app/components/Header/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="relative h-75 bg-gradient-to-br from-indigo-400 via-purple-400 to-blue-300 dark:from-indigo-800 dark:via-purple-800 dark:to-blue-700  flex items-center justify-between px-8 md:px-20 py-20">
+    <section className="relative h-75 bg-gradient-to-br from-blue-400 via-purple-300 to-indigo-400 dark:from-indigo-800 dark:via-purple-800 dark:to-blue-700  flex items-center justify-between px-8 md:px-20 py-20">
       <div>
         <h1 className="text-5xl font-bold mb-2">Jamie Smart</h1>
         <h2 className="text-2xl opacity-90 mb-6">Software Engineer</h2>
